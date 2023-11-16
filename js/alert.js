387028a2051d3730fac641d9efd0a503e61eb47f
PatzyPazty <138332686+PatzyPazty@users.noreply.github.com>
2023-11-16 21:13:23 +0700

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,30 +1,110 @@
 document.getElementById('Wildan').addEventListener('click', function () {
-    swal({
-      buttons: {
-        confirm: "Ok",
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Wildan.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
       },
-      content: {
-        element: "img",
-        attributes: {
-          src: "./popup/Wildan.png", // Ganti dengan URL gambar Anda
-          style: "max-width: 300px;"
-        },
-      },
-    })
-  });
+    },
+  })
+});
 
+document.getElementById('Irgy').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Irgy.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
+      },
+    },
+  })
+});
 
-  document.getElementById('Sultan').addEventListener('click', function () {
-    swal({
-      buttons: {
-        confirm: "Ok",
+document.getElementById('Sherin').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Sherin.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
       },
-      content: {
-        element: "img",
-        attributes: {
-          src: "./popup/Sultan.png", // Ganti dengan URL gambar Anda
-          style: "max-width: 300px;"
-        },
+    },
+  })
+});
+
+document.getElementById('Aisyah').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Aisyah.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
       },
-    })
+    },
+  })
+});
+
+document.getElementById('Fathih').addEventListener('click', function () {
+  swal({
+    buttons: {
+      cancel: "Tidak Sekarang",
+      confirm: "Kunjungi Personal Website",
+    },
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Fathih.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
+      },
+    },
+  }).then((value) => {
+    if (value) {
+      window.open("./About Developer/index.html", "_blank");
+    }
   });
+});
+
+document.getElementById('Sultan').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Sultan.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
+      },
+    },
+  })
+});
+
+document.getElementById('Raidan').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Raidan.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
+      },
+    },
+  })
+});
+
+document.getElementById('Yoga').addEventListener('click', function () {
+  swal({
+    buttons: false,
+    content: {
+      element: "img",
+      attributes: {
+        src: "./popup/Yoga.png", // Ganti dengan URL gambar Anda
+        style: "max-width: 100%;"
+      },
+    },
+  })
+});
